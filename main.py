908544046b998490a23522e1ba5631e6c37d1bd0
Habibah Adam <hebibe2s@gmail.com>
2021-11-20 13:53:36 +1100

    write to scv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import csv
 
 
 def tile_exist(link):
@@ -19,16 +20,24 @@ def write_to_csv(link):
         price_dollar = tile.find(name="span", class_="box--value")
         price_cent = tile.find(name="span", class_="box--decimal")
         unit_price = tile.find(name="span", class_="box--baseprice")
+
+        
+        product_row = []
         if product_name:
-            print(product_name.getText())
+            product_row.append(product_name.getText().strip())
         if product_img:
-            print(product_img)
+            product_row.append(product_img)
         if price_dollar and price_cent:
-            print(f"{price_dollar.getText()}{price_cent.getText()}")
+            product_row.append(f"{price_dollar.getText().strip()}{price_cent.getText().strip()}")
         if unit_price:
-            print(unit_price.getText())
+            product_row.append(unit_price.getText().strip())
         else:
-            print("no unit price")
+            product_row.append("no unit price")
+
+
+        with open("products_info.csv", "a") as f:
+            writer = csv.writer(f)
+            writer.writerow(product_row)
 
 
 response = requests.get("https://www.aldi.com.au/en/groceries/")
